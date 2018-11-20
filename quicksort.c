45755f62c1aaa41afa7e591434d693d5517b799f
Gabscurioshop <33849234+Gabscurioshop@users.noreply.github.com>
2018-11-19 19:42:32 -0500

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,57 +1,29 @@
 #include <stdio.h>
-#include <stdbool.h>
-//partition function                                                                               
-int partition(int arr[], int left, int right){
-  int l = left;
-  int r = right;
-  int mid = left + (right-left)/2;
-  int piv = arr[mid];//pivot position                                                              
-  int temp = 0;
-  bool done = false;
-  while(!done){
-    while(arr[l] < piv){
-      l++;//left value inceases                                                                    
-    }
-    while(piv < arr[r]){
-      r--;//right value decreases                                                                  
-    }
-    if(l >= r) done = true;
-    else{
-      temp = arr[l];
-      arr[l] = arr[r];
-      arr[r] = temp;
-      l++;
-      r--;
-    }
-  }//end of done while loop                                                                        
-  return r;
-}
+#include <stdlib.h>
 
-//quick sort function                                                                              
-void quickSort(int arr[], int left, int right){
-  int mid = 0;//midpoint                                                                           
-  if(left >=right) return;//if there's less than 2 elements, array is sorted                       
-  mid = partition(arr, left, right);
-  quickSort(arr, left, mid);//sorts low partition                                                  
-  quickSort(arr, mid+1, right);//sorts high partition                                              
+void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
+int comparator(const void * p, const void * q){
+  return(*(int*)p - *(int*)q);
 }
 
 int main(){
-  FILE *input;//text file                                                                          
-  int num;//stores number from text file                                                           
-  int vals[5000];
-  int c = 0;//array counter                                                                        
-  while(!feof(input)){//while the text file isn't empty                                            
-    fscanf(input, "%d", &num);//scans number from text file                                        
-    vals[c] = num;
-    c++;
-  }//end of while loop                                                                             
-  for(int i = 0; i<c-1; i++){
-    printf("%d\n\n", vals[i]);
+  FILE *input;//text file                                                                           
+  int k;
+  int val[] = {234, 32, 543, 636, 75, 8567, 54, 33, 78, 8976, 43, 312, 13, 243};//will replace with reading from a file
+  int size = sizeof(val) / sizeof(val[0]);
+  printf("Before sorting the array: \n");
+  for(int i = 0; i < size; i++){
+    printf("%d\n", val[i]);
   }
-  printf("Quick Sort Result is: ");
-  quickSort(vals, 0, c-2);
-  for(int i = 0; i<c-1; i++){
-    printf("%d\n\n", vals[i]);
-  }//prints integers in quick sort order                                                           
+  printf("Input a k: \n");
+  scanf("%d", &k);
+  qsort(val, size, sizeof(int), comparator);//sorts the integers using quicksort                    
+  printf("\nQuick sort array is: \n");
+  for(int i = 0; i<size; i++){
+    printf("%d\n", val[i]);//prints the integers in ascending order                                 
+  }
+  printf("\nkth smallest integer: \n");
+  printf("%d\n", val[k-1]);//prints the kth integer after quick sorting                             
+  return(0);
+
 }
