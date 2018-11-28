bbba6f5c69d145828c6e7c822549f01c693784ad
Gabscurioshop <33849234+Gabscurioshop@users.noreply.github.com>
2018-11-28 18:06:01 -0500

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -7,12 +7,15 @@ int comparator(const void * p, const void * q){
 }
 
 int main(){
-  FILE *input = fopen("test14.txt", "r");//text file                                                                           
-  int k;
+   int k;
   int size = 14;
   int val[size];
-  //int val[] = {234, 32, 543, 636, 75, 8567, 54, 33, 78, 8976, 43, 312, 13, 243};//will replace with reading from a file
-  //int size = sizeof(val) / sizeof(val[0]);
+
+  printf("Input a k: \n");
+  scanf("%d", &k);
+  FILE *input = fopen("test14.txt", "r");//text file                                                                                                           
+  //int val[] = {234, 32, 543, 636, 75, 8567, 54, 33, 78, 8976, 43, 312, 13, 243};//will replace with reading from a file                                      
+  //int size = sizeof(val) / sizeof(val[0]);                                                                                                                   
   for(int i = 0; i< size; i++){
     fscanf(input, "%d", &val[i]);
   }
@@ -20,16 +23,15 @@ int main(){
   for(int i = 0; i < size; i++){
     printf("%d\n", val[i]);
   }
-  printf("Input a k: \n");
-  scanf("%d", &k);
-  qsort(val, size, sizeof(int), comparator);//sorts the integers using quicksort                    
+  qsort(val, size, sizeof(int), comparator);//sorts the integers using quicksort                                                                               
   printf("\nQuick sort array is: \n");
   for(int i = 0; i<size; i++){
-    printf("%d\n", val[i]);//prints the integers in ascending order                                 
+    printf("%d\n", val[i]);//prints the integers in ascending order                                                                                            
   }
   printf("\nkth smallest integer: \n");
-  printf("%d\n", val[k-1]);//prints the kth integer after quick sorting
+  printf("%d\n", val[k-1]);//prints the kth integer after quick sorting                                                                                        
   fclose(input);
-  return(0);
+ return(0);
 
 }
+
